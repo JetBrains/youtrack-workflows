def00b2feedbca1e847cbad0456a14b291061b19
Mariya Davydova <mariyafomkina@gmail.com>
2017-12-04 18:01:54 +0300

    Generate New Issues: parent submitted and schedule

---
+++ js/generating-new-issues/newsletter-every-month.js
@@ -0,0 +1,49 @@
+var entities = require('v1/entities');
+
+var MONTHS = [
+  "January", "February", "March", "April",
+  "May", "June", "July", "August",
+  "September", "October", "November", "December"
+];
+
+var DAY_IN_MS = 24 * 60 * 60 * 1000;
+
+exports.rule = entities.Issue.onSchedule({
+  title: 'Internal Newsletter',
+  search: '#DA-1', // anchor issue; it is required to ensure that
+  // this rule is executed exactly once according to schedule
+  cron: '0 0 19 15 1/1 ? *', // on the 15th day of every month at 19:00
+  action: function(ctx) {
+    var date = new Date(Date.now() + 31 * DAY_IN_MS);
+    date.setDate(1);
+    var month = MONTHS[date.getMonth()];
+    var year = date.getFullYear();
+    
+    var newIssue = new entities.Issue(ctx.currentUser, ctx.issue.project,
+      month + ' ' + year + ' Internal Newsletter');
+    newIssue.fields.Assignee = ctx.author;
+    newIssue.fields.Subsystem = ctx.Subsystem.Newsletters;
+    newIssue.fields.DD = date.getTime();
+  },
+  requirements: {
+    Assignee: {
+      type: entities.User.fieldType
+    },
+    Subsystem: {
+      type: entities.OwnedField.fieldType,
+      Newsletters: {}
+    },
+    DD: {
+      type: entities.Field.dateType,
+      name: 'Due Date'
+    },
+    author: {
+      type: entities.User,
+      login: 'root'
+    },
+    da: {
+      type: entities.Issue,
+      id: 'DA-1'
+    }
+  }
+});
---
+++ js/generating-new-issues/when-parent-is-submitted.js
@@ -0,0 +1,40 @@
+var entities = require('@jetbrains/youtrack-scripting-api/entities');
+
+exports.rule = entities.Issue.onChange({
+  title: 'Release Management',
+  guard: function(ctx) {
+    var issue = ctx.issue;
+    return issue.becomesReported &&
+      issue.fields.Type.name === ctx.Type.Release.name;
+  },
+  action: function(ctx) {
+    var issue = ctx.issue;
+    
+    var createIssue = function(name, subsystem) {
+      var newIssue = new entities.Issue(ctx.currentUser, issue.project,
+        name + ' for ' + issue.summary);
+      newIssue.fields.Subsystem = subsystem;
+      newIssue.fields.Type = ctx.Type.Task;
+    };
+    
+    createIssue('Update documentation', ctx.Subsystem.Documentation);
+    createIssue('Update "What\'s new"', ctx.Subsystem.Site);
+    createIssue('Blog post', ctx.Subsystem.Blog);
+    createIssue('Newsletter for customers', ctx.Subsystem.Newsletters);
+    createIssue('Newsletter for resellers', ctx.Subsystem.Newsletters);
+  },
+  requirements: {
+    Type: {
+      type: entities.EnumField.fieldType,
+      Release: {},
+      Task: {}
+    },
+    Subsystem: {
+      type: entities.OwnedField.fieldType,
+      Documentation: {},
+      Site: {},
+      Blog: {},
+      Newsletters: {}
+    }
+  }
+});
